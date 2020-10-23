eb2a47135738e75f82d37c70c86029305e256250
Tanishq Gupta <tanishqg2406@gmail.com>
2020-10-23 11:58:04 +0530

    minor change

--- mergesort.h
+++ mergesort.h
@@ -8,13 +8,6 @@
 using namespace std;
 
 
-void mergeSeq(pSort::dataType *ar1, int n1, pSort::dataType *ar2, int n2, pSort::dataType* ext);
-void mergeSortSeq(pSort::dataType *data, pSort::dataType *ext, int l, int r);
-
-void mergePar(vector<int>& ind, int l_sz, int procN[], MPI_Datatype& pSortType, 
-                pSort::dataType *data, pSort::dataType *left, pSort::dataType *right, 
-                pSort::dataType *ans);
-
 void mergeSortPar(int procN[], int numProcs, int maxSz, pSort::dataType *data, int ndata, 
                     int ID, MPI_Datatype& pSortType);
 
